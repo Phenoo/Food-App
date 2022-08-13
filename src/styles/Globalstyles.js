290d01495dc18de0d5bbbcb103102ab1294f9dce
phenopreneur <54966250+phenopreneur@users.noreply.github.com>
2022-08-14 01:45:10 +0200

    second commit

--- src/styles/Globalstyles.js
+++ src/styles/Globalstyles.js
@@ -1,10 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 
-const GlobalStyle = createGlobalStyle`
+const GlobalStyle = createGlobalStyle `
 *, *::before, *::after{
   margin: 0,
   padding: 0
+}
+
+html{
+  overflow-x: hidden;
 }
 
 body{
